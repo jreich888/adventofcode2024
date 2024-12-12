5aa761d0550bd735d5f4efa35f453a3af2bff7b0
Jason Reich <jreich@xreich.com>
2024-12-12 12:40:05 -0800

    some day 12 cleanup.

--- src/day_12.rs
+++ src/day_12.rs
@@ -9,18 +9,16 @@ use std::collections::HashSet;
 fn get_cell_at(plot_map:&Vec<Vec<char>> ,dims:(i32, i32),x:i32,y:i32) -> char {
     // either get elev at coord, or -1 if off map
     let row =plot_map.get(y as usize);
-    if row == None { return '.'; }
-    return *row.unwrap().get(x as usize).unwrap_or(&'.');
-    
+    match row {
+        None => return '.',
+        _ => return *row.unwrap().get(x as usize).unwrap_or(&'.')
+    }
 }
+
 
 fn expand_region(plot_map:&Vec<Vec<char>> ,dims:(i32, i32),x:i32,y:i32, crop: char, region: &mut HashSet<(i32,i32)>) {
 
-
-
-
     // println!("expand region: {x} {y} {crop}");
-        
 
     for dx in -1..2 {
         // could skip check but I like it
@@ -43,10 +41,9 @@ fn expand_region(plot_map:&Vec<Vec<char>> ,dims:(i32, i32),x:i32,y:i32, crop: ch
             }
         }
     }
-
-
-
 }
+
+
 
 fn find_region(plot_map:&Vec<Vec<char>> ,dims:(i32, i32),x:i32,y:i32) -> HashSet<(i32,i32)> {
     let letter = get_cell_at(plot_map, dims,x,y);
@@ -97,22 +94,28 @@ fn is_edge(region: &HashSet<(i32,i32)>, x:i32, y:i32, side:i32 ) -> bool {
     return !region.contains(&(x+dx,y+dy));
 }
 
+
+// Key to P2...
+// for any side, the side ID will be the neigboring inline side with the minimum
+// X or Y.  So three top sides in a row will have the same side_id (hash key) all
+// with the X of the leftmost coordinate
 fn calc_side_id(region: &HashSet<(i32,i32)>, x:i32, y:i32, stype:i32) -> (i32,i32,i32) {
     // println!("calc_side_id {x} {y} {stype}");
     let mut dx = 0;
     let mut dy = 0;
+    // for this side type, are we iterating up or left?
     if stype==LEFT || stype==RIGHT { dy = -1; } else { dx=-1; }
 
     let mut minx = x;
     let mut miny = y;
-
     loop {
-        // see if cell above has side
+        // see if cell above has adjacent side
         // println!("   calc_side_id {minx} {miny} {dx} {dy} {stype}");
-        let adjacent = region.contains(&(minx+dx,miny+dy));
+        let adjacent_cell = region.contains(&(minx+dx,miny+dy));
         let adjacent_edge = is_edge(region,minx+dx,miny+dy,stype);
         // println!("       calc_side_id adjacent {adjacent}  adjacent_edge {adjacent_edge}");
-        if adjacent && adjacent_edge {
+        if adjacent_cell && adjacent_edge {
+            // continue looking in direction
             minx += dx;
             miny += dy;
         } else { break; }
@@ -125,7 +128,6 @@ fn calc_side_id(region: &HashSet<(i32,i32)>, x:i32, y:i32, stype:i32) -> (i32,i3
 
 fn calc_sides( region: &HashSet<(i32,i32)> ) -> i32 {
     // perimeter is sum of sides of cells without neighbors in each
-
     // each side is notated by it's min-x, min-y, and T/L/R/B
 
     let mut sides : HashSet<(i32,i32,i32)> = HashSet::new();
@@ -150,8 +152,6 @@ pub fn process_lines(lines:Vec<String>) -> u64 {
     let mut count_p1 = 0;
     let mut count_p2 = 0;
 
-    
-
     // iterate the chars and determine the size of the entire FS
     // Size of FS is sum of numbers
     for l in lines {
@@ -160,11 +160,13 @@ pub fn process_lines(lines:Vec<String>) -> u64 {
     }
 
     for r in plot_map.clone() {
-        // println!("{:?}", r);
+        let rs: String = r.iter().collect();
+        // println!("{rs}");
     }
+
     let dims: (i32, i32) = (plot_map[0].len() as i32, plot_map.len() as i32);
 
-    
+    if false 
     {
         let region00 = find_region(&plot_map,dims,0,0);
         let region_area = region00.len() as i32;
@@ -172,52 +174,36 @@ pub fn process_lines(lines:Vec<String>) -> u64 {
         let region_sides = calc_sides(&region00);
 
         println!("region00 area {} perim {} sides {}  ALL {:?}", region_area, region_perim, region_sides, region00);
+        return 0u64;
     }
-
-    // return 0u64;
-
-    let mut found_plots : HashSet<(i32,i32)> = HashSet::new();
 
     let mut total_cost =0;
 
+    // union of found regions so far
+    let mut found_plots : HashSet<(i32,i32)> = HashSet::new();
 
+    // iterate looking for regions
+    for y in 0..dims.1 {
+        for x in 0..dims.0 {
+            if found_plots.contains(&(x,y)) {continue;}
 
-        // iterate looking for regions
-        for y in 0..dims.1 {
-            for x in 0..dims.0 {
-                if found_plots.contains(&(x,y)) {continue;}
+            let region = find_region(&plot_map,dims,x,y);
+            found_plots.extend(region.clone());
 
+            let plant = get_cell_at(&plot_map, dims, x, y);
+            let region_area = region.len() as i32;
+            let region_perim = calc_perim(&region);
+            let region_sides = calc_sides(&region);
 
-                let region = find_region(&plot_map,dims,x,y);
-                found_plots.extend(region.clone());
+            // P1
+            // let cost = region_area*region_perim;
+            // p2
+            let cost = region_area*region_sides;
+            total_cost += cost;
 
-                let plant = get_cell_at(&plot_map, dims, x, y);
-                let region_area = region.len() as i32;
-                let region_perim = calc_perim(&region);
-                let region_sides = calc_sides(&region);
-
-                let cost = region_area*region_sides;
-                total_cost += cost;
-
-                println!("found region {plant} area {region_area} perim {region_perim} sides {region_sides} cost {cost}");
-
-                
-
-
-                // if (topo_map[y as usize][x as usize] == 0) {
-                //     // println!("found trailhead at {x},{y}");
-                //     // let trail_count = count_trails(&mut topo_map, x, y, dims);
-                //     // println!("found trailhead at {x},{y} trails={} {}", trail_count.0, trail_count.1);
-                //     // count_p1 += trail_count.1;
-                //     // count_p2 += trail_count.0;
-                // }
-            }
+            println!("found region {plant} area {region_area} perim {region_perim} sides {region_sides} cost {cost}");
         }
-
-        
-
-
+    }
     
    return total_cost as u64;
-
 }
